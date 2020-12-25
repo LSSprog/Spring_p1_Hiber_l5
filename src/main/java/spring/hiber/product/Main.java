1cb65080569538ee0d7963d0646947a9594a7158
LSS <fedexss@bk.ru>
2020-12-25 19:59:01 +0300

    домашнее задание к 5 уроку

---
+++ src/main/java/spring/hiber/product/Main.java
@@ -0,0 +1,22 @@
+package spring.hiber.product;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Factory factory = new Factory();
+
+        ProductDao dao = new ProductDao(factory);
+
+        dao.showManyItems();
+
+        List<Product> allProducts = dao.findAll();
+        Product product = dao.findById(3L);
+        dao.saveOrUpdate(new Product("trust", 131));
+        dao.deleteById(4L);
+
+        factory.shutdown();
+
+    }
+}
--- src/main/java/spring/hiber/product/ProductDao.java
+++ src/main/java/spring/hiber/product/ProductDao.java
@@ -3,27 +3,69 @@ package spring.hiber.product;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ProductDao {
-    private static SessionFactory factory;
+    private static Factory factory; //почему я здесь статик написал?...
 
-    public static void init() {
-
-        factory = new Configuration().configure("config/product/hibernate.cfg.xml").buildSessionFactory();
+    @Autowired // пытался проинжектить сюда фабрику - чего то не вышло :(((
+    public void setFactory(Factory factory) {
+        this.factory = factory;
     }
 
-    public static void showManyItems() {
-        try (Session session = factory.getCurrentSession()){
+    public ProductDao(Factory factory) {
+        this.setFactory(factory);
+    }
+
+    /*public void showManyItems() {
+        try (Session session = factory.getSession()) { //getCurrentSession()){
             session.beginTransaction();
             List<Product> products = session.createQuery("FROM products.products_tbl").getResultList();
             System.out.println(products + "/n");
         }
+    }*/
+
+    public Product findById(Long id) {
+        try (Session session = factory.getSession()) { //getCurrentSession()){
+            session.beginTransaction();
+            //Product pr1 = (Product) session.byId(String.valueOf(id)); // это ищет по id???
+            Product pr2 = session.get(Product.class, id); // или это ???
+            Product pr = session.createQuery(String.format(
+                    "SELECT p FROM products_tbl WHERE p.id = %d", id), Product.class)
+                    .getSingleResult(); // через запрос поиск
+            return pr2; // pr1 pr можно попробовать вернтуь
+
+        }
+
     }
 
-    public static void main(String[] args) {
-        init();
-        showManyItems();
+    public List<Product> findAll() {
+        try (Session session = factory.getSession()) { //getCurrentSession()){
+            session.beginTransaction();
+            List<Product> products = session.createQuery("FROM products_tbl").getResultList();
+            return products;
+        }
+    }
+
+    public void deleteById(Long id) {
+        try (Session session = factory.getSession()) { //getCurrentSession()){
+            session.beginTransaction();
+            Product product = session.get(Product.class, id);
+            session.delete(product); // или здесь можно написать запроц query на удаление, чтобы оъект не вытаскивать (из лекции)
+            session.getTransaction().commit();
+        }
+
+    }
+
+    public void saveOrUpdate (Product product) {
+        try (Session session = factory.getSession()) { //getCurrentSession()){
+            session.beginTransaction();
+            session.saveOrUpdate(product);
+            session.getTransaction().commit();
+        }
     }
 }
